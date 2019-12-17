8c5309b0e99b261bf1c4f83482427e6631851788
AVDEY <a-777-aaa@mail.ru>
2019-12-17 16:13:11 +0700

    simple manage hibernate over JPA

--- REGISTRATION/src/main/java/ru/register/shop/manage/ManageShop.java
+++ REGISTRATION/src/main/java/ru/register/shop/manage/ManageShop.java
@@ -8,6 +8,9 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.register.shop.domain.Shop;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.io.Serializable;
 import java.util.List;
 
@@ -17,6 +20,34 @@ public class ManageShop {
     public static void main(String[] args) {
 
 
+       exampleHibernate();
+
+        exampleJPA();
+
+    }
+
+    private static void exampleJPA() {
+
+        EntityManagerFactory  factory = Persistence.createEntityManagerFactory("persistence");
+        EntityManager entityManager=factory.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Shop shop = new Shop();
+        shop.setName("GAZ");
+
+        entityManager.persist(shop);
+        System.out.println(shop.getShoppingID());
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+        entityManager=factory.createEntityManager();
+        List fromShop = entityManager.createQuery("FROM Shop ").getResultList();
+        fromShop.forEach(o -> System.out.println(o));
+        entityManager.close();
+
+    }
+
+    private static void exampleHibernate() {
         SessionFactory sessionBuild = buildSessionFactory();
 
         System.out.println();
@@ -27,34 +58,25 @@ public class ManageShop {
 
 
         Session session = sessionBuild.openSession();
-
         session.getTransaction().begin();
-
         session.getTransaction().commit();
 
         Shop shop = new Shop();
-
         shop.setName("Подкова");
-
         Long id = (Long) session.save(shop);
 
         System.out.println(id);
-
         session.close();
-
-
 
         session = sessionBuild.openSession();
         Shop shopName = session.get(Shop.class, id);
         System.out.println(shopName.toString());
         session.close();
 
-
         session = sessionBuild.openSession();
         List<Shop> list = session.createQuery("FROM Shop", Shop.class).list();
         session.close();
         list.forEach(shop1 -> System.out.println(shop1));
-
     }
 
 
