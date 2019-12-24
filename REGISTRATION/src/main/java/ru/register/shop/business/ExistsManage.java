3bfdab4a89319ccada9c1eef15b583aa7ac77297
AVDEY <a-777-aaa@mail.ru>
2019-12-24 15:58:14 +0700

    add Spring Data JPA

--- REGISTRATION/src/main/java/ru/register/shop/business/ExistsManage.java
+++ REGISTRATION/src/main/java/ru/register/shop/business/ExistsManage.java
@@ -14,10 +14,12 @@ import ru.register.shop.dao.ShopDao;
 import ru.register.shop.domain.BigShop;
 import ru.register.shop.domain.RegisterCertificate;
 import ru.register.shop.domain.Shop;
+import ru.register.shop.domain.SmallShop;
 import ru.register.shop.view.StoreAddressRequest;
 import ru.register.shop.view.StoreAddressResponse;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service("manageService")
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
@@ -41,19 +43,35 @@ public class ExistsManage {
     @Transactional()
     public StoreAddressResponse findRegisterShop(StoreAddressRequest request){
         LOGGER.info("findRegisterShop CALLED");
-        RegisterCertificate certificate = shopDAO.findCertificate(request);
+      //  RegisterCertificate certificate = shopDAO.findCertificate(request);
 
         dao.findShops();
 
-        dao.addShop(newShop());
-        dao.addShop(newShop());
-        dao.addShop(newShop());
-        
+        dao.addShop(newShop(1));
+        dao.addShop(newShop(2));
+
+
+        RegisterCertificate certificate = getCertificate();
+
+        shopDAO.saveAndFlush(certificate);
+
+        //shopDAO.findAll();
+
+        shopDAO.findById(1L);
+
+        List<RegisterCertificate> certificates = shopDAO.findAll();
+        for (RegisterCertificate registerCertificate:certificates){
+            System.out.println(registerCertificate);
+        }
+
         return new StoreAddressResponse();
     }
 
-    private Shop newShop() {
-        Shop shop=new BigShop();
+
+    private Shop newShop(int uid) {
+
+        Shop shop= uid == 1 ? new SmallShop() : new BigShop();
+
         shop.setName("Гастроном");
         shop.setInn(125404);
         shop.setDateCreate(LocalDate.of(1999 , 10 , 13));
@@ -61,4 +79,26 @@ public class ExistsManage {
     }
 
 
+    private RegisterCertificate getCertificate(){
+
+        RegisterCertificate rc = new RegisterCertificate();
+        rc.setNumber("AA22RUSABVGD");
+        rc.setIssueDate(LocalDate.now());
+        rc.setActive(true);
+
+        List<Shop> shops = dao.findShops();
+
+        for (Shop shop: shops){
+
+            if(shop instanceof BigShop){
+                rc.setShop(shop);
+            }
+            if(shop instanceof SmallShop){
+                rc.setShop(shop);
+            }
+
+        }
+
+       return rc;
+    }
 }
--- REGISTRATION/src/main/java/ru/register/shop/dao/ExistsShopDAO.java
+++ REGISTRATION/src/main/java/ru/register/shop/dao/ExistsShopDAO.java
@@ -1,49 +1,18 @@
 package ru.register.shop.dao;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.register.shop.domain.RegisterCertificate;
-import ru.register.shop.view.StoreAddressRequest;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 /**
- * существует ли такой магазин запрос в бд
+ * существует ли такой магазин запрос в бд через JPA
  */
-@Component
-public class ExistsShopDAO {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExistsShopDAO.class);
-
-    private EntityManager entityManager;
-
-    @Value("${test.value}")
-    private String test;
-
-
-    /*
-    public ExistsShopDAO() {
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistence");
-        entityManager = factory.createEntityManager();
-    }*/
+@Repository
+public interface ExistsShopDAO extends JpaRepository<RegisterCertificate , Long> {
 
 
 
-    public RegisterCertificate findCertificate(StoreAddressRequest request) {
-        LOGGER.info("findCertificate : " + test);
-        //TODO изменю далее
-        return null;
-    }
 
-
-    public void setTest(String test) {
-        this.test = test;
-    }
 
 
 }
